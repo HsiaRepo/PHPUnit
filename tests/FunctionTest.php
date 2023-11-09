b5da366843767d5a81f7e6356b655bc4f6975129
HsiaRepo <logan@hsia.us>
2023-11-09 12:56:51 -0500

    test(add-test): added test cases for correct add function

---
+++ tests/FunctionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class FunctionTest extends TestCase
+{
+    public function testAddReturnsCorrectSum()
+    {
+        require 'functions.php';
+
+        $this->assertEquals(4, add(2, 2));
+        $this->assertEquals(10, add(5, 5));
+
+    }
+}
